060ccfe953cfc8a97757487bf42db4507a4fad2b
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-09-17 00:17:52 +0100

    Movement and Jump physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+    private bool isGrounded;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+    
+    //FixedUpdate is called once every physics update, not affected by FPS, 100 a second default
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput,rigidbodyComponent.velocity.y,0);
         
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+        
+        if (jumpKeyWasPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 5,ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
     }
 }
