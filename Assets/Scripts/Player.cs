9d3bdcb49f55c364650b138b93f7207fce988786
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-09-17 17:34:17 +0100

    Added Score and endflag

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Transform groundCheckTransform;
     [SerializeField] private LayerMask playerMask;
+    [SerializeField] Material powerup;
+    [SerializeField] Material player;
     private bool jumpKeyWasPressed;
     private float horizontalInput;
+    private float verticalInput;
     private Rigidbody rigidbodyComponent;
-    
+    private int superJumpsRemaining;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,13 +32,14 @@ public class Player : MonoBehaviour
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
     
     //FixedUpdate is called once every physics update, not affected by FPS, 100 a second default
     private void FixedUpdate()
     {
-        rigidbodyComponent.velocity = new Vector3(horizontalInput * 3,rigidbodyComponent.velocity.y,0);
-        
+        rigidbodyComponent.velocity = new Vector3(horizontalInput * 2,rigidbodyComponent.velocity.y,verticalInput * 2);
+
         if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
         {
             return;
@@ -40,8 +47,33 @@ public class Player : MonoBehaviour
         
         if (jumpKeyWasPressed)
         {
-            rigidbodyComponent.AddForce(Vector3.up * 7,ForceMode.VelocityChange);
+            float jumpPower = 5f;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 1.5f;
+                superJumpsRemaining--;
+            }
+            transform.GetComponent<Renderer>().material = player;
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower,ForceMode.VelocityChange);
             jumpKeyWasPressed = false;
         }
+        
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+            transform.GetComponent<Renderer>().material = powerup;
+            GameObject.Find("Score Text").GetComponent<Score>().score++;
+        }
+        
+        if (other.gameObject.layer == 7)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
 }
